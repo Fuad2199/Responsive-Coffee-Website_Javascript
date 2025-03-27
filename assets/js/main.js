08b1572c265f946495ae86308a5578122c3cb67d
Fuad2199 <fuad.beybutov099@gmail.com>
2025-03-27 21:42:05 +0400

    popular section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-// ============== SHOW MENU ==============
+
+// ============== NAV MENU STARTS ==============
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
@@ -6,16 +7,17 @@ const navMenu = document.getElementById('nav-menu'),
 /* Menu show */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
+        navMenu.classList.add('nav--show-menu')
     })
 }
 
 /* Menu hidden */
 if(navClose){
     navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
+        navMenu.classList.remove('nav--show-menu')
     })
 }
+// ============== NAV MENU STARTS ==============
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
@@ -23,7 +25,7 @@ const navLink = document.querySelectorAll('.nav__link')
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+    navMenu.classList.remove('nav--show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
@@ -31,8 +33,23 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const shadowHeader = () =>{
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('shadow-header') 
-                       : header.classList.remove('shadow-header')
+    this.scrollY >= 50 ? header.classList.add('header--shadow') 
+                       : header.classList.remove('header--shadow')
 }
 window.addEventListener('scroll', shadowHeader)
 
+// ================= SWIPER POPULAR ==================
+
+const swiperPopular = new Swiper('.popular__swiper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 32,
+    slidesPerView: 'auto',
+    centeredSlices: 'auto',
+
+    breakpoints: {
+        1150: {
+            spaceBetween: 80,
+        }
+    }
+  });
